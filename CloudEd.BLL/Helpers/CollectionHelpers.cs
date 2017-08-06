d27a509b3634dbe1c651e662c31175526298353b
mitutee <volman1285@gmail.com>
2017-08-06 12:25:54 +0300

    Added helper for collections

---
+++ CloudEd.BLL/Helpers/CollectionHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudEd.BLL.Helpers
+{
+    public static class CollectionHelpers
+    {
+        public static T RandomElement<T>(this IEnumerable<T> collection)
+        {
+            var r = new Random(DateTime.Now.Millisecond);
+            return collection.ElementAt(r.Next(0, collection.Count()));
+        }
+    }
+}
